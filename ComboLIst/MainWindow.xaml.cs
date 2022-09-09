34e9711fe6b21859e2c90d58f4c0c4a345741a76
gothamlies <shaneez.osu@gmail.com>
2022-09-09 10:49:08 +0500

    Добавьте файлы проекта.

---
+++ ComboLIst/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComboLIst
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Button button = new Button();
+            button.Content = "gg4";
+            comboBox.Items.Add("Hello");
+            comboBox.Items.Add("World");
+            comboBox.Items.Add("!");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.Text.Length == 0) return;
+            string newItem = textBox.Text.Trim();
+            if (comboBox.Items.Contains(newItem))
+            {
+                MessageBox.Show("элемент с таким именем уже существует");
+                return;
+            }
+            comboBox.Items.Add(newItem);
+            textBox.Text = "";
+
+        }
+
+        private void orty_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.Text.Length == 0) return;
+            {
+                textBox.Text = "";
+            }
+
+            ComboBox comboBox = sender as ComboBox;
+            if (comboBox == null) return;
+            {
+                comboBox.Items.Clear();
+            }
+        }
+    }
+}
+
